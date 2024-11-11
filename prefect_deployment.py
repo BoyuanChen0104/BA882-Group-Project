b37d955d38c16c9ccb9cb38485f93aeec977272f
Boyuan Chen <bychan@bu.edu>
2024-11-11 16:53:22 -0500

    update the deployment of prefect

--- prefect_deployment.py
+++ prefect_deployment.py
@@ -5,7 +5,7 @@ from prefect_flow import weekly_workflow
 deployment = Deployment.build_from_flow(
     flow=weekly_workflow,
     name="weekly_scripts_deployment",
-    schedule=CronSchedule(cron="0 1 * * 2"),  #Execute every Tuesday 1 AM
+    schedule=CronSchedule(cron="0 1 * * 2"),  # Run at 1 AM every Tuesday
     work_queue_name="default"
 )
 
